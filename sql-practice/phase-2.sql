45fcf439f0ab0cf371b459a89f8eb91b1c810bc1
Taylor Rossmann <taylor0rossmann@gmail.com>
2024-01-10 18:31:31 -0500

    finished the tables

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,14 @@
--- Your code here 
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(10),
+    phone INTEGER(10),
+    email VARCHAR(255),
+    created_at CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE coffee_orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    is_redeemed BOOLEAN DEFAULT 'not redeemed',
+    ordered_at CURRENT_TIMESTAMP NOT NULL
+);
